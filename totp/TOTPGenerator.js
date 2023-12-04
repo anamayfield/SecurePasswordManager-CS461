dc20dc327514ab1129a529df50edac743771e7aa
anamayfield <97195887+anamayfield@users.noreply.github.com>
2023-12-04 10:54:01 -0600

    Updated TOTPGenerator with real TOTP logic and dependencies

--- totp/TOTPGenerator.js
+++ totp/TOTPGenerator.js
@@ -7,9 +7,17 @@ const generateTOTP = (secret) => {
   });
 };
 
-// Generate a test secret key
-const secret = speakeasy.generateSecret({length: 20}).base32;
+const verifyTOTP = (token, secret) => {
+  // Implement the logic to verify the TOTP
+  return speakeasy.totp.verify({ secret, encoding: 'base32', token, window: 1 });
+};
 
-// Test the generateTOTP function
-const testTOTP = generateTOTP(secret);
-console.log(`Generated TOTP: ${testTOTP}`);
+
+// // Generate a test secret key
+// const secret = speakeasy.generateSecret({length: 20}).base32;
+
+// // Test the generateTOTP function
+// const testTOTP = generateTOTP(secret);
+// console.log(`Generated TOTP: ${testTOTP}`);
+
+module.exports = { generateTOTP, verifyTOTP };
