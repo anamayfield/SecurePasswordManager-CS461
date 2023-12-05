c4399c4729ca9f53cc702ecb146c940763153be8
jparker32 <104544456+jparker32@users.noreply.github.com>
2023-12-05 12:28:05 -0800

    Add basic functionality for v0.0.2

--- passwd_store_and_encr/main.js
+++ passwd_store_and_encr/main.js
@@ -4,20 +4,57 @@ const port = 8000;
 
 app.use(express.json());
 
+let users = [];
+let currentLoginId = 1;
+
 app.post('/create-login', (req, res) => {
-    res.status(501).send('Not Implemented: Create Login');
+    const { username, password, notes } = req.body;
+
+    if (!username || !password || !notes) {
+        return res.status(400).send('Invalid body');
+    }
+
+    users.push({ loginId: currentLoginId, username: username, password: password, notes: notes });
+    currentLoginId += 1;
+    res.status(201).send('User creation successful');
 });
 
-app.post('/read-logins', (req, res) => {
-    res.status(501).send('Not Implemented: Read Logins');
+app.post('/read-logins', (_req, res) => {
+    res.json(users);
 });
 
 app.post('/update-login', (req, res) => {
-    res.status(501).send('Not Implemented: Update Login');
+    const { loginId, username, password, notes } = req.body;
+
+    if (!loginId || !username || !password || !notes) {
+        return res.status(400).send('Invalid body');
+    }
+
+    const usr = users.findIndex(user => (user.loginId === loginId));
+
+    if (usr === -1) {
+        return res.status(404).send('User not found');
+    }
+
+    users[usr] = { loginId: loginId, username: username, password: password, notes: notes };
+    res.status(201).send('User update successful');
 });
 
 app.post('/delete-login', (req, res) => {
-    res.status(501).send('Not Implemented: Delete Login');
+      const { loginId } = req.body;
+
+      if (!loginId) {
+        return res.status(400).send('Invalid body');
+      }
+
+      const usr = users.findIndex(user => user.loginId === loginId);
+
+      if (usr === -1) {
+          return res.status(404).send('User not found');
+      }
+
+      users.splice(usr, 1);
+      res.status(200).send('User deletion successful');
 });
 
 app.listen(port, () => {
