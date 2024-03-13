14873ae3c9303c0c7da002a0505a22563ea7b1c9
Audrie Nielsen <audrienielsen@Audries-Air.sys>
2024-03-12 21:48:22 -0700

    Editing index.js to fix issue with deployment

--- password-manager/src/index.js
+++ password-manager/src/index.js
@@ -1,6 +1,5 @@
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const fs = require('fs');
 
 const App = require('./App').default;
 
@@ -8,4 +7,3 @@ const html = ReactDOMServer.renderToString(
   React.createElement(React.StrictMode, null, React.createElement(App))
 );
 
-fs.writeFileSync('output.html', `<!DOCTYPE html>${html}`);
