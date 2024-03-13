b9d949541ebebe749e269f68de60a1e4b2b65bd3
Ana Mayfield <anamayfield@outlook.com>
2024-03-12 22:16:06 -0500

    Add TOTP verification functionality

--- totp/routes/server.js
+++ totp/routes/server.js
@@ -1,23 +1,20 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '../../../.env' });
+import { config } from 'dotenv';
 import cors from 'cors';
+import express, { json } from 'express';
+import sendEmailRouter from './sendEmail.js';
+import verifyTOTPRouter from './verifyTOTP.js';
 
-
-import express from 'express';
-import sendEmailRouter from './sendEmail.js'; // Adjust the path as necessary
-
-
+config({ path: '../../../.env' });
 
 const app = express();
 
 app.use(cors());
+app.use(json());
 
-app.use(express.json());
-
-// Mount the sendEmail router on /api/totp
 app.use('/api/totp', sendEmailRouter);
+app.use('/api/verify-totp', verifyTOTPRouter);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
---
+++ totp/routes/verifyTOTP.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+const router = Router();
+import { createClient } from '@supabase/supabase-js';
+import { verifyTOTP, decryptSecret } from '../TOTPGenerator.js';
+
+// Initialize Supabase client
+const supabaseUrl = 'https://dtwmtlfnskzbtsgndetr.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0d210bGZuc2t6YnRzZ25kZXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE4ODQxMTMsImV4cCI6MjAxNzQ2MDExM30.qoYr-kxeXb4I3rRe-dzqaC__SWiAUt4g1YSsES01mxk';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+router.post('/', async (req, res) => {
+    const { totpCode, userId } = req.body;
+    console.log("Received userId for verification:", userId); // Add this line
+
+    try {
+        // Fetch the TOTP code for the user from your database
+        const { data, error } = await supabase
+            .from('totp')
+            .select('totp_code') // Now fetching the encrypted secret key
+            .eq('user_id_string', userId)
+            .single();
+
+        if (error) throw error;
+
+        if (data) {
+            const secretKey = decryptSecret(data.totp_code); // Decrypt the secret key
+            const isValid = verifyTOTP(totpCode, secretKey); // Verify using the decrypted secret key
+            
+            if (isValid) {
+                res.json({ verified: true, message: 'TOTP verification successful.' });
+            } else {
+                res.status(401).json({ verified: false, message: 'Invalid TOTP code.' });
+            }
+        } else {
+            res.status(404).json({ message: 'User not found.' });
+        }
+    } catch (error) {
+        console.error('Error verifying TOTP:', error);
+        res.status(500).json({ message: 'An error occurred during TOTP verification.' });
+    }
+});
+
+export default router;
