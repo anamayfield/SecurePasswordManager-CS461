c78291e2dc36e31381f271a0e6027586df84b436
Audrie Nielsen <audrienielsen@Audries-Air.sys>
2024-03-12 21:59:29 -0700

    Fixing it again because I broke it

--- password-manager/src/index.js
+++ password-manager/src/index.js
@@ -1,9 +1,17 @@
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
+import React from 'react';
+ import ReactDOM from 'react-dom/client';
+ import './index.css';
+ import App from './App';
+ import reportWebVitals from './reportWebVitals';
 
-const App = require('./App').default;
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>
+ );
 
-const html = ReactDOMServer.renderToString(
-  React.createElement(React.StrictMode, null, React.createElement(App))
-);
-
+ // If you want to start measuring performance in your app, pass a function
+ // to log results (for example: reportWebVitals(console.log))
+ // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ reportWebVitals();
