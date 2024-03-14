6fb40bddcace15209ad28ae64e9a7c0108a7f944
Audrie Nielsen <audrienielsen@Audries-MacBook-Air.local>
2024-03-13 23:05:59 -0700

    changing instance of userId to parentId

--- password-manager/src/HandleSignOut.js
+++ password-manager/src/HandleSignOut.js
@@ -4,7 +4,7 @@ export const handleSignOut = async (navigate, cookies) => {
   try {
     const supabase = createSupaClient();
     await signOut(supabase);
-    cookies.remove('userId');
+    cookies.remove('parentId');
     navigate('/login');
   } catch (error) {
     console.error('Error signing out:', error.message);
