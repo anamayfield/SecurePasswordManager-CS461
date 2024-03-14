538f089908d6459a8df57ec48525d25df5d345f1
Audrie Nielsen <audrienielsen@Audries-Air.sys>
2024-03-13 18:07:33 -0700

    bug fix

--- password-manager/src/Dashboard.js
+++ password-manager/src/Dashboard.js
@@ -52,7 +52,7 @@ const Dashboard = () => {
     };
 
     fetchData();
-  }, [navigate, userParentId]);
+  }, [navigate, parentId]);
 
   useEffect(() => {
     const filtered = passwords.filter((password) =>
